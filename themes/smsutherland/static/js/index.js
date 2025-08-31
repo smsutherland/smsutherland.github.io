f13ee4eaae37ce66eaa36a1638a4e5ae49b69032
Sagan Sutherland <sagan@farthinghalearms.com>
2025-08-31 00:27:02 -0400

    add link to turn off uwu mode.

--- themes/smsutherland/static/js/index.js
+++ themes/smsutherland/static/js/index.js
@@ -1,11 +1,5 @@
 window.addEventListener("load", () => {
   const url = new URL(window.location);
-  if (url.searchParams.get("uwu")) {
-    let script = document.createElement("script");
-    script.src = "/js/uwu.js";
-    script.onload = () => uwuifyPage();
-    document.head.append(script);
-  }
 
   if (url.searchParams.size > 0) {
     let params = url.searchParams;
@@ -14,7 +8,7 @@ window.addEventListener("load", () => {
       if (anchors[i].host !== url.host) {
         continue;
       }
-      anchor = new URL(anchors[i].href);
+      let anchor = new URL(anchors[i].href);
       for (const [key, value] of url.searchParams) {
         if (anchor.searchParams.get(key) === null) {
           anchor.searchParams.set(key, value);
@@ -22,6 +16,22 @@ window.addEventListener("load", () => {
       }
         anchors[i].href = anchor.toString();
     }
+  }
+
+  if (url.searchParams.get("uwu")) {
+    let script = document.createElement("script");
+    script.src = "/js/uwu.js";
+    script.onload = () => uwuifyPage();
+    document.head.append(script);
+
+    let footer = document.getElementById("footer");
+    let credit = footer.children[1];
+    let unuwu = document.createElement("div");
+    let unuwu_url = new URL(window.location);
+    unuwu_url.searchParams.delete("uwu");
+    unuwu.innerHTML = `Youwu've found uwu mode :3. Click <a href="${unuwu_url.toString()}">here</a> to turn it off. ;-;`;
+    unuwu.style = "width: 10em;";
+    footer.insertBefore(unuwu, credit);
   }
 });
 
